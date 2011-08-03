ae7cd0be41b3175e01aeabb836487d7c4b2d4c19
Max Ogden <mogden@gmail.com>
2011-08-02 20:46:18 -0400

    add validate doc update

--- app.js
+++ app.js
@@ -35,6 +35,9 @@ ddoc.validate_doc_update = function (newDoc, oldDoc, userCtx) {
   if (newDoc._deleted === true && userCtx.roles.indexOf('_admin') === -1) {
     throw "Only admin can delete documents on this database.";
   }
+  if (oldDoc && userCtx.roles.indexOf('_admin') === -1) {
+    throw "Only admin can modify documents on this database.";
+  }
 };
 
 couchapp.loadAttachments(ddoc, path.join(__dirname, 'attachments'));
